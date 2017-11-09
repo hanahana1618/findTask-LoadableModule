14411c2f891e0642e6aa91ba8cc92593face0203
Ivette Castro ivetto@Ivetto_Mac.local <hanahana1.618@hotmail.es>
2017-11-09 12:02:29 -0500

    Took out unnecessary code

--- findTask.c
+++ findTask.c
@@ -3,16 +3,15 @@
 #include <linux/kernel.h>    // Contains types, macros, functions for the kernel - printk()
 #include <linux/errno.h>     // error codes
 #include <linux/sched.h>     //scheduler for the kernel, include the PID info  
-#include <linux/string.h>    //include string compare      
+#include <linux/string.h>    //include string compare on line 34     
 
 MODULE_LICENSE("GPL");       ///< The license type -- this affects runtime behavior
 MODULE_AUTHOR("Prieto Castro");       ///< The author -- visible when you use modinfo
 MODULE_DESCRIPTION("A Linux loadable module that checks is a process is running and reports it. Written for Embedded OS at FIU.");  ///< The description -- see modinfo
 MODULE_VERSION("0.1");              ///< The version of the module
 
-static char *name = "init";   ///< An example LKM argument -- default value is "world"
+static char *name = "init";   ///< An example LKM argument -- default value is the process with PID 1 because it exists in every computer
 struct task_struct *task;
-//unsigned char flag = 0;
 
 module_param(name, charp, S_IRUGO);
 ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
@@ -26,28 +25,18 @@ MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parame
  *  @return returns 0 if successful
  */
 static int __init hello_init(void){
-   printk(KERN_INFO "TDR: Finding process %s in list of running processes\n", name);
-
-   //task = current->mm;
+   printk(KERN_INFO "Finding process %s in list of running processes\n", name);
 
    for_each_process(task) {
       if(strcmp(task->comm, name)) {
-	printk(KERN_INFO "Found process with PID %d", task->pid);
-        //flag = 1;
-        //break;
+	       printk(KERN_INFO "Found process with PID %d", task->pid);
+         //if process ID found then exit out of the program
         return 0;
       }
-      //else {
-      //  printk(KERN_INFO "" );
-      //}
-
-      //printk("TASK.COMM: Current running process %s\n", task->comm);
    }
 
-   
    printk(KERN_INFO "Not Found %s", name);
-   
-
+  
    return 0;
 }
 
@@ -57,7 +46,7 @@ static int __init hello_init(void){
 *  code is used for a built-in driver (not a LKM) that this function is not required.
 */
 static void __exit hello_exit(void){
-   printk(KERN_INFO "TDR: Goodbye from the Test LKM!\n");
+   printk(KERN_INFO "Module unloaded\n");
 }
 
 /** @brief A module must use the module_init() module_exit() macros from linux/init.h, 
